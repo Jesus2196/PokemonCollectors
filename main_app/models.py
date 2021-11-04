6e3c8a419694be5ebf6af7c615d0e48b34c4bdfa
Jesus <Jesuspineda9512@gmail.com>
2021-11-04 08:19:04 -0700

    Training table implementation

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.urls import reverse
 
 # Create your models here.
 
+EXERCISES = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('E', 'Evening')
+)
+
 
 class Pokemon(models.Model):
     name = models.CharField(max_length=100)
@@ -18,5 +24,15 @@ class Pokemon(models.Model):
 
 
 class Training(models.Model):
-    date = models.DateField()
-    exercise = models.CharField(max_length=1)
+    date = models.DateField('Training Date')
+    exercise = models.CharField(
+        'Meal Period',
+        max_length=1,
+        choices=EXERCISES,
+        default=EXERCISES[0][0]
+    )
+
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.get_exercise_display()} on {self.date}.'
